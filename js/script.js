4a5d7a971844d597a6dbe7d742489d296cf1f04e
ElenaRuscitto <ruscitto.elena@gmail.com>
2024-03-05 15:35:33 +0100

    [add] bonus

--- js/script.js
+++ js/script.js
@@ -1,39 +1,54 @@
 
+const output = document.querySelector('.output');
 const teams = [
   {
-    foto: 'img/wayne-barnett-founder-ceo.jpg',
+    foto: 'wayne-barnett-founder-ceo.jpg',
     nome: 'Wayne Barnett',
     ruolo: 'Founder & CEO'
   },
 
   {
-    foto: 'img/angela-caroll-chief-editor.jpg',
+    foto: 'angela-caroll-chief-editor.jpg',
     nome: 'Angela Caroll',
     ruolo: 'Chief Editor'
   },
 
   {
-    foto: 'img/walter-gordon-office-manager.jpg',
+    foto: 'walter-gordon-office-manager.jpg',
     nome: 'Walter Gordon',
     ruolo: 'Office Manager'
   },
 
   {
-    foto: 'img/angela-lopez-social-media-manager.jpg',
+    foto: 'angela-lopez-social-media-manager.jpg',
     nome: 'Angela Lopez',
     ruolo: 'Social Media Manager'
   },
 
   {
-    foto: 'img/scott-estrada-developer.jpg',
+    foto: 'scott-estrada-developer.jpg',
     nome: 'Scott Estrada',
     ruolo: 'Developer'
   },
 
   {
-    foto: 'img/barbara-ramos-graphic-designer.jpg',
+    foto: 'barbara-ramos-graphic-designer.jpg',
     nome: 'Barbara Ramos',
     ruolo: 'Graphic Designer'
   }
 
-]
+];
+
+for (let people of teams) {
+  output.innerHTML += `
+  <div class="col mt-4 mb-3">
+  <div class="card">
+    <img src="img/${people.foto}" alt="${people.nome}">
+    <div class="card-body">
+      <h5 class="card-title">${people.nome}</h5>
+      <p class="card-text mb-2">${people.ruolo}</p>
+    </div>
+  </div>
+</div>
+  `
+}
